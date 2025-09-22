6bd726e0fd580ac31340a3db85bd215a9e425a42
srishti soni <srishtisoni7541@gmail.com>
2025-09-22 19:18:52 +0530

    first commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const BASE_URL =
   import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+  console.log(BASE_URL);
 
 const api = axios.create({
   baseURL: BASE_URL,
